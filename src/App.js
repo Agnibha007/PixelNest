15cf8dcd426b8cee2b1eebbe435b40c1ad99e881
Agnibha Mukherjee <yuzonestudios@gmail.com>
2026-03-05 16:45:28 +0530

    pixel

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Home from './pages/Home';
-import About from './pages/About';
-import Services from './pages/Services';
-import Testimonials from './pages/Testimonials';
-import Contact from './pages/Contact';
-import './styles/App.css';
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Services from "./pages/Services";
+import Testimonials from "./pages/Testimonials";
+import Contact from "./pages/Contact";
+import "./styles/App.css";
 
 function App() {
-  const theme = 'dark';
+  const theme = "dark";
 
   useEffect(() => {
-    document.documentElement.setAttribute('data-theme', theme);
+    document.documentElement.setAttribute("data-theme", theme);
   }, [theme]);
 
   return (
